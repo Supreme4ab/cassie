69855ec5cdae41ea0322cd8d34de724c831b29fc
Supreme4ab <supreme.4ab@gmail.com>
2025-05-18 15:19:59 +0100

    Update CommonUtil.lua

--- Main/Modules/CommonUtil.lua
+++ Main/Modules/CommonUtil.lua
@@ -1,1 +1,19 @@
+local Services = setmetatable({}, {
+    __index = function(self, service)
+        local s = game:GetService(service)
+        rawset(self, service, s)
+        return s
+    end
+})
 
+local CommonUtil = {}
+
+function CommonUtil.GetService(name)
+    return Services[name]
+end
+
+function CommonUtil.GetLocalPlayer()
+    return Services.Players.LocalPlayer
+end
+
+return CommonUtil
